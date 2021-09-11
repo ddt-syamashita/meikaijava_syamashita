ffc46572e88370885d880489739dd3568b3fd738
ddt-syamashita <ddt-syamashi@gmail.com>
2021-09-11 15:28:25 +0900

    2021/09/011 レビュー指摘対応 進捗率100％

--- meikai_java/src/E_9_01/E_9_01.java
+++ meikai_java/src/E_9_01/E_9_01.java
@@ -29,22 +29,6 @@ public class E_9_01 {
 		}
 	}
 
-	static class Human {
-		// 人間クラスの名前を定義する
-		String name = "";
-		// 人間クラスの身長を定義する
-		int height = 0;
-		// 人間クラスの体重を定義する
-		int weight = 0;
-
-		// 名前の変数に代入させるため、引数ありのコンストラクタを定義する
-		Human(String name, int height, int weight) {
-			this.name = name;
-			this.height = height;
-			this.weight = weight;
-		}
-	}
-
 	public static void main(String[] args) {
 
 		// Scannerクラスをインスタンス化
