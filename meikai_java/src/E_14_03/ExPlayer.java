44e564abbd1deda4c94e85ec733fd5161f4e20c9
ddt-syamashita <ddt-syamashi@gmail.com>
2021-12-18 22:36:52 +0900

    2021/12/18 E_14_03 進捗率100％

---
+++ meikai_java/src/E_14_03/ExPlayer.java
@@ -0,0 +1,8 @@
+package E_14_03;
+
+//　拡張プレーヤ インタフェース（スロー再生付き）
+public interface ExPlayer extends Player {
+	
+	//スロー再生メソッド
+	void slow();
+}
---
+++ meikai_java/src/E_14_03/Player.java
@@ -0,0 +1,11 @@
+package E_14_03;
+
+// プレーヤ インタフェース
+public interface Player {
+
+	//再生メソッド
+	void play();
+	
+	//停止メソッド
+	void stop();
+}
