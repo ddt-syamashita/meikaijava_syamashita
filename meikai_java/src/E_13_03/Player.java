20de9e1939dcf3c2bc08d7d516e87d002e805df0
ddt-syamashita <ddt-syamashi@gmail.com>
2022-02-11 22:51:32 +0900

    2022/2/11 E_13_03 指摘箇所修正 進捗率100%

--- meikai_java/src/E_13_03/Player.java
+++ meikai_java/src/E_13_03/Player.java
@@ -13,6 +13,18 @@ public abstract class Player {
 	/**
 	 * 出した手をString型の文字列で返却する抽象メソッド
 	 */
-	public abstract String getHand();
+	public String getHand() {
+
+		// handの値が0だった場合
+		if (hand == 0) {
+			return "「グー」 を出しました。";
+			// handの値が1だった場合
+		} else if (hand == 1) {
+			return "「チョキ」　を出しました。";
+			// handの値が0と1以外だった場合
+		} else {
+			return "「パー」 を出しました。";
+		}
+	}
 
 }
